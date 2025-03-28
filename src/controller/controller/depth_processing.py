245f2ce830de60ca2fb0a3c8f9a5cb1ff395cf10
DanielTSimpson <daniel@undertheoak.net>
2025-03-28 14:33:14 -0400

    Got some object tracking to work off min point, but the line sometimes goes off frame

--- src/controller/controller/depth_processing.py
+++ src/controller/controller/depth_processing.py
@@ -37,42 +37,46 @@ def crop(data, top = 0, right = 0, bottom = 0, left = 0):
 
 
 
-def show_image(data, window_name="Filtered Image", wait_time = 0, max_width=1600, max_height=1200, colorize = True):
+def show_img(data, node, loss = 0, window_name="Filtered Image", wait_time = 0, max_width=1600, max_height=1200, colorize = True):
 
     # Normalize the data for visualization (if necessary)
     normalized_data = np.uint8(data)  # Convert to 8-bit image
     if colorize:
         normalized_data = cv2.normalize(normalized_data, None, 0, 255, cv2.NORM_MINMAX)
         normalized_data = cv2.applyColorMap(normalized_data, cv2.COLORMAP_JET)
-
+    
     # Get original dimensions
     height, width = normalized_data.shape[:2]
-
-    # Calculate scaling factor to fit within max dimensions while preserving aspect ratio
-    scale = min(max_width / width, max_height / height)
-
-    # Resize the image to fit the window
-    new_width, new_height = int(width * scale), int(height * scale)
-    resized_image = cv2.resize(normalized_data, (new_width, new_height))
-
-    # Create resizable window and display the image
-    cv2.namedWindow(window_name, cv2.WINDOW_AUTOSIZE)
-    resized_image = cv2.cvtColor(resized_image, cv2.COLOR_RGB2BGR) # Convert RGB to BGR
     
-    cv2.resizeWindow(window_name, new_width, new_height)
-    cv2.imshow(window_name, resized_image)
+    #Visualize loss with a horizontal line
+    if loss:
+        node.get_logger().info(str(loss))
+        cv2.line(normalized_data, (width//2, height//2), (width//2 + loss, height//2), (255, 0, 0), 1)
 
-    # Wait for a key press and then close the window
-    cv2.waitKey(wait_time)
-    cv2.destroyAllWindows()
+        # Calculate scaling factor to fit within max dimensions while preserving aspect ratio
+        scale = min(max_width / width, max_height / height)
+
+        # Resize the image to fit the window
+        new_width, new_height, new_loss = int(width * scale), int(height * scale), int(loss * scale)
+        resized_image = cv2.resize(normalized_data, (new_width, new_height))
+
+        # Create resizable window and display the image
+        cv2.namedWindow(window_name, cv2.WINDOW_AUTOSIZE)
+        resized_image = cv2.cvtColor(resized_image, cv2.COLOR_RGB2BGR) # Convert RGB to BGR
+        cv2.resizeWindow(window_name, new_width, new_height)
+        cv2.imshow(window_name, resized_image)
+
+        # Wait for a key press and then close the window
+        cv2.waitKey(wait_time)
+        cv2.destroyAllWindows()
 
 
 
 def filter_img(data, node):   
     data_rows = np.size(data, 0)  # Number of Rows
     data_cols = np.size(data, 1)  # Number of Columns
-    K_rows = data_rows // data_rows * 2  #Filter window height. Must be factor of data_rows
-    K_cols = data_cols // data_cols * 2  #Filter window width. Must be factor of data_cols
+    K_rows = data_rows // data_rows * 8  #Filter window height. Must be factor of data_rows
+    K_cols = data_cols // data_cols * 8  #Filter window width. Must be factor of data_cols
     
     # Filter Matrix (for averaging)
     K = np.ones((K_rows, K_cols)) / (K_rows * K_cols)
@@ -88,12 +92,8 @@ def filter_img(data, node):
                 # Perform element-wise multiplication and sum the result for averaging
                 R[j // K_rows, k // K_cols, i] = np.sum(sub_arr * K)
     
+    return R
     
-   # cv2.imshow("Picture", R)
-    show_image(R, colorize = False)
-    #cv2.imshow('Image', bgr_array)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
 
 
 def filter_depth(data, node):    
@@ -114,17 +114,22 @@ def filter_depth(data, node):
 
             # Perform element-wise multiplication and sum the result for averaging
             R[i // K_rows, j // K_cols] = np.sum(sub_arr * K)
+   
+    loss = int(np.argmin(R) - np.size(R, 1) / 2) * K_cols
+    node.get_logger().info("Plot Loss: " + str(int(np.argmin(R)) - (np.size(R, 1)) / 2) +
+            "\tTrue Loss: " + str(loss)
+            )
     
     #Plot data array
     plot_data(R[0])
     
-    loss = int(np.argmin(R)) - data_cols // 2 // K_cols
     
     return Int32(data = loss)
 
 
 
 class DepthProcessor(Node):
+
     def __init__(self):
         super().__init__('DepthProcessor')
         
@@ -148,20 +153,21 @@ class DepthProcessor(Node):
         timer_period = 0.05
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
-
-
     def callback1(self, msg):
         bridge = CvBridge()
         depth_image = bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
         data = np.asarray(depth_image, dtype="int32")
         data = crop(data, 0, 0, 480//2, 24)
         self.loss = filter_depth(data, self)
+        self.get_logger().info("Got loss: " + str(self.loss))
 
     def callback2(self, msg):
         bridge = CvBridge()
         color_img = bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
         data = np.asarray(color_img, dtype="uint8")
-        #filter_img(data, self)
+        color_img = filter_img(data, self)
+        if self.loss:
+            show_img(color_img, self, loss = self.loss.data, colorize = False)
 
     def timer_callback(self):
         if self.loss:
